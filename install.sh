2b9846039de36beaa1a943d2bf7c1fe47b297e9b
felix <thefelancholicalone@gmail.com>
2026-04-22 17:56:23 +0300

    Полностью переписал скрипт установки на человеческий. Теперь он использует символические ссылки для файлов конфигов. Он создаёт папку config в рабочей директории исполняемого скрипта, после чего на основе выбранных настроек создаёт файлы конфигов. Также добавлена поддержка void linux(сам на нём сижу).

--- install.sh
+++ install.sh
@@ -1,46 +1,158 @@
 #!/bin/bash
+LCONFIG_DIR="$(pwd)/config"
+DEFAULT_DIR="$(pwd)/default"
+CONFIG_FILES=("i3" "alacritty" "polybar" "rofi" "wal" "picom")
+CONFIG_DIR="$HOME/.config"
+BACKUP_SFX=".backup-$(date +%Y%m%d_%H$M$S)"
+I3_CFG="$LCONFIG_DIR/i3/config.d"
 
-read -p "Начать установку? (yes/no): " decision
-read -p "Использовать клавишу win для запуска rofi? (По умолчанию win+d): (yes/no): " usewin
+PACKAGES="i3 picom rofi pywal scrot xclip yad alacritty polybar feh dolphin"
 
-if [ "$decision" = "yes" ]; then
+echo "Сейчас тебе и3 ставить будем"
+echo "Твои старые файлы конфигов удалены не будут,они будут в том же месте,но с с прeпиской .backup"
+read -p "Начать установку? (y/n): " decision
+read -p "Использовать клавишу win для запуска rofi? (По умолчанию win+d): (y/n): " usewin
+
+
+symlink() {
+	
+	local src="$1"
+	local dst="$2"
+	
+	if [ -L "$dst" ] && [ "$(readlink "$dst")" = "$dst" ]; then
+		echo "уже есть ссылка: $dst -> $src"
+		return
+	fi
+
+	if [ -e "$dst" ] && [ ! -L "$dst" ]; then
+		echo "создаю backup для $dst"
+		mv "$dst" "$dst$BACKUP_SFX"
+	fi
+
+	ln -sf "$src" "$dst"
+	echo "создана ссылка: $dst -> $src"
+}
+
+get_distro() {
+local distro="$(grep '^ID_LIKE=' /etc/os-release | cut -d'=' -f2 | tr -d '"')"
+
+if [ "$distro" = "" ]; then 
+	distro="$(grep '^ID=' /etc/os-release | cut -d'=' -f2 | tr -d '"')"
+	fi
+
+	echo "$distro"
+}
+
+add_or_replace() {
+	local srch="$1"
+	local rplc="$2"
+	local dst="$3"
+	
+	if ! grep -q "$srch"  "$dst" ; then 
+	echo "$rplc" >> "$dst" 
+	return
+	else
+		sed -i "s|$srch|$rplc|" "$dst"
+	fi
+
+
+}
+
+install_packages() {
+	local pkgs="$1"
+	local distro="$(get_distro)"
+
+	echo " система: "$distro" "
+
+	case "$distro" in 
+		*arch*) 
+			echo "Проверяем,установлен ли yay..."
+			
+			if ! pacman -Qi "yay" &> /dev/null; then
+				echo "yay не установлен. Устанавливаем..."
+				sudo pacman -S --needed git base-devel
+				git clone https://aur.archlinux.org/yay-bin.git
+				cd yay-bin
+				makepkg -si	
+				cd .. ||
+				rm -rf yay-bin
+				fi
+			echo "устанавливаем недостающие пакеты..."
+			yay -S --needed --noconfirm $pkgs
+			;;
+		*void*) 
+			echo "Устанавливаем недостающие пакеты..."
+			sudo xbps-install -Sy $pkgs
+			;;
+	esac
+} 
+
+
+#starting installatiom
+if [[ "$decision" =~ "y" ]]; then	
     echo "Понадобятся права администратора, йоу"
     sudo -v
 
-    echo "Устанавливаем все необходимые пакеты..."
-    PACKAGES="i3-wm alacritty rofi i3status polybar picom pywal feh scrot xclip dolphin"
-    [ "$usewin" = "yes" ] && PACKAGES="$PACKAGES xcape"
-    
-    yay -S --needed --noconfirm $PACKAGES && echo "Пакеты установлены успешно."
+#setting up congig/ files
+	if [ -n "$LCONFIG_DIR" ]; then
+		read -p "папка config не была пуста. Удалить содержимое? (y/n)" clear_config
+		if [[ "$clear_config" =~ "y" ]]; then
+			rm -rf $LCONFIG_DIR
+			mkdir -p $LCONFIG_DIR
+		else
+			echo "Так дела не идут. Убери что осталось в папке $LCONFIG_DIR и тогда можем поговорить"
+			exit 1
+		fi
 
-    echo "Удаляю старые файлы конфигов, если таковые имеются..."
-    for folder in i3 rofi alacritty polybar wal; do
-        if [ -d "$HOME/.config/$folder" ]; then
-            rm -rf "$HOME/.config/$folder"
-            echo "Папка $folder удалена."
-        fi
-    done
-    
-    echo "Копирую файлы конфигов в ~/.config..."
-    
-    configs=("i3" "alacritty" "rofi" "polybar" "wal")
-    for cfg in "${configs[@]}"; do
-        if [ -d "./$cfg" ]; then
-            cp -R "./$cfg" ~/.config/ && echo "Конфиг $cfg скопирован успешно."
-        else
-            echo "Ошибка: Исходная папка ./$cfg не найдена!"
-        fi
-    done
+	fi
 
-    if [ "$usewin" = "yes" ]; then
-        echo "Настраиваю запуск rofi по одиночному нажатию Win..."
-        # Добавляем в автозагрузку
-        echo "exec --no-startup-id xcape -e 'Super_L=Alt_L|F1'" >> ~/.config/i3/config.d/autostart.conf
-        # Меняем бинд в конфиге (Alt_L+F1 — это Alt+F1, который эмулирует ksuperkey)
-        sed -i 's/bindcode $mod+40/bindsym Mod1+F1/g' ~/.config/i3/config.d/keybinds.conf
+	cp -R $DEFAULT_DIR/* $LCONFIG_DIR
+
+#checking if distro is void and prompting to set up sound
+
+    if [[ "$(get_distro)" =~ "void" ]]; then
+
+	read -p "У тебя void linux. Добавить звук в автозапуск i3? (pipewire,pipewire-pulse,wireplumber) Они также будут установлены,если их не было. (y/n): " install_sound
+	if [[ "$install_sound" =~ "y" ]]; then
+
+	PACKAGES="$PACKAGES wireplumber pipewire" 	
+
+	add_or_replace "exec --no-startup-id pipewire" "exec --no-startup-id pipewire" "$I3_CFG/autostart.conf"
+	add_or_replace "exec --no-startup-id pipewire-pulse" "exec --no-startup-id pipewire-pulse" "$I3_CFG/autostart.conf"
+	add_or_replace "exec --no-startup-id wireplumber" "exec -no-startup-id wireplumber" "I3_CFG/autostart.sh"
+	fi
+
     fi
 
-    echo "Установка завершена! Перезагрузите i3 (Mod+Shift+R)."
+    [[ "$usewin" =~ "y" ]] && PACKAGES="$PACKAGES xcape"
+
+#install
+ 
+echo "Устанавливаем все необходимые пакеты..."
+
+    install_packages "$PACKAGES"
+
+#symlinking
+
+    echo "Создаём символические ссылки..."
+   
+    for cfg in "${CONFIG_FILES[@]}"; do 
+	    symlink "$LCONFIG_DIR/$cfg" "$CONFIG_DIR/$cfg"
+    done
+
+#windows key 
+
+if [[ "$usewin" =~ "y" ]]; then
+        echo "Настраиваю запуск rofi по одиночному нажатию Win..."
+        
+	echo "exec --no-startup-id xcape -e 'Super_L=Alt_L|F1'" >> "$I3_CFG/autostart.conf"
+        
+	sed -i 's/bindcode \$mod+40/bindsym Mod1+F1/g' "$I3_CFG/autostart.conf"
+    fi
+
+#finish
+
+    echo "Установка завершена! Перезайди в i3 (Mod+Shift+M)."
 else
     echo "Ну и иди ты"
 fi
